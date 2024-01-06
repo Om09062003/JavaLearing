a086099fcc41f494a50b2a181610e9c43d3c419f
Om09062003 <svpt12omugale65@gmail.com>
2024-01-06 22:40:57 +0530

    Add new paragraph

---
+++ AllFunction.java
@@ -0,0 +1,84 @@
+import java.util.Scanner;
+
+public class AllFunction {
+
+//Main
+    //public static void main(String[] args){
+        /* myMethod("Ramesh");
+        myMethod("Nisha");
+        myNumber();
+        System.out.println(sum(10 )); */
+        //doMultiple();
+        /* System.out.print("Enter The Number: ");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        myFactorial(n); */
+
+    //} 
+
+//Print Name Greeting
+    /* public static void myMethod(String Name) {
+        System.out.println("Good Morning "+ Name) ;       
+    } */
+
+//Add 2 Numbers
+    /* public static void myNumber(){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the first number: ");
+        int a = sc.nextInt();
+        System.out.print("Enter the second number: ");
+        int b = sc.nextInt();
+        int sum = a+b;
+        System.out.println("Sum: "+ sum);
+    } */
+
+//Mulyiply 2 numbers
+    /* public static void doMultiple(){
+        Scanner sc = new Scanner (System.in);
+        System.out.print("Enter The First Number: ");
+        int a = sc.nextInt();
+        System.out.print("Enter The Second Number: ");
+        int b = sc.nextInt();
+        int multiple = a*b;
+        System.out.print("Your Product: "+multiple);
+    } */
+
+    
+//Add First 1 0 Numbers
+    /* public static int sum(int k){
+        if(k>0){
+            return k+sum(k-1);
+        }else{
+            return 0;
+        } 
+    } */
+
+//Print name
+    /* public static void main(String[] args){
+        myName();
+    }
+
+    public static void myName(){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Your Name: ");
+        String name = sc.nextLine();
+        System.out.println("My Name Is: "+name);
+        return;
+    } */
+
+//Factorial
+    /* public static void myFactorial(int n){
+        int Factorial = 1;
+        if(n<0){
+            System.out.println("Invalid");
+            return;
+        }else{
+            for(int i=n; i>=1; i--){
+                Factorial = Factorial * i;
+            }
+            System.out.println("Factorial: "+Factorial);
+        }
+    } */
+}
+
+
