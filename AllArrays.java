5399eccb07af1329ec8c3f6930b92aeee71d8a2a
Om09062003 <svpt12omugale65@gmail.com>
2024-01-20 13:17:27 +0530

    Add new paragraph

--- AllArrays.java
+++ AllArrays.java
@@ -50,6 +50,13 @@ public class AllArrays {
         /* int num[]={2,4,6,8,10};
         pairArray(num); */
 
+    //Print Subarray
+        /* int num[] = {2,4,6,8,10};
+        printSubarray(num); */
+
+    //MAX Subarraay sum
+        /* int num[] = {2,4,6,8,10};
+        maxSubarray(num); */
     } 
 
 //Liner Search
@@ -118,5 +125,49 @@ public class AllArrays {
             System.out.println();
         }
         System.out.println("Total Piars: "+tp);
+    } */ 
+
+//Print Subarray
+    /* public static void printSubarray(int num[]){
+        int ts=0;
+        for(int i=0; i<num.length; i++){
+            for(int j=i; j<num.length;j++){
+                System.out.print("(");
+                int sum=0;
+                for(int k=i;k<=j;k++){
+                    System.out.print(num[k]+", ");
+                    sum+=num[k];
+                }
+                ts++;
+                System.out.print(")"+" ");
+            }
+            System.out.println();
+        }
+        System.out.println("Total Subarray: "+ts);
+    } */
+
+//MAX Subarray Sum
+    /* public static void maxSubarray(int num[]){
+        int ts=0;
+        int sum=0;
+        int maxSum = Integer.MIN_VALUE;
+        for(int i=0; i<num.length; i++){
+            for(int j=i; j<num.length;j++){
+                System.out.print("(");
+                sum=0;
+                for(int k=i;k<=j;k++){
+                    sum+=num[k];
+                }
+                System.out.print(sum);
+                ts++;
+                System.out.print(")"+" ");
+            }
+            if(maxSum<sum){
+                maxSum = sum;
+            }
+            System.out.println();
+        }
+        System.out.println("Max Sum Is: "+maxSum);
+        System.out.println("Total Subarray: "+ts);
     } */ 
 }
