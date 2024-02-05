e69074420255d560e6f07d8c617d393c743b4af4
Om09062003 <svpt12omugale65@gmail.com>
2024-02-06 00:12:25 +0530

    Add new paragraph

---
+++ Arrays/InsertionSort.java
@@ -0,0 +1,28 @@
+package Arrays;
+
+public class InsertionSort {
+    public static void insertionSort(int num[]){
+        for(int i=0; i<num.length; i++){
+            int curr = num[i];
+            int prev = i-1;
+            while(prev >=0 && num[prev]>curr){
+                num[prev+1] = num[prev];
+                prev--;
+            }
+            num[prev+1]=curr;
+        }
+    }
+
+    public static void printArr(int num[]){
+        for(int i=0; i<num.length; i++){
+            System.out.print(num[i]+" ");
+        }
+    }
+
+    public static void main(String[] args){
+        int num[] = {5, 4, 1, 3, 2};
+        System.out.println(num[2-1]);
+        insertionSort(num);
+        printArr(num);
+    }
+}
