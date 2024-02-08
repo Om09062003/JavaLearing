afc0ed6f74c38a6ff3ea0409cfb925c646704120
Om09062003 <svpt12omugale65@gmail.com>
2024-02-08 16:17:30 +0530

    Add new paragraph

--- Arrays2D/BasicMatrix.java
+++ Arrays2D/BasicMatrix.java
@@ -3,6 +3,19 @@ package Arrays2D;
 import java.util.Scanner;
 
 public class BasicMatrix {
+
+    public static boolean searchKey(int matrix[][], int key){
+        for(int i=0; i<matrix.length; i++){
+            for(int j=0; j<matrix[0].length; j++){
+                if(matrix[i][j]==key){
+                    System.out.println("found at cell ("+i+","+j+")");
+                    return true;
+                }
+            }
+        }
+        System.out.println("Key not found");
+        return false;
+    }
     public static void main(String[] args){
         int matrix[][] = new int[3][3];
         Scanner sc = new Scanner(System.in);
@@ -13,11 +26,13 @@ public class BasicMatrix {
         }
 
         //output
+        searchKey(matrix, 5);
         for(int i=0; i<matrix.length; i++){
             for(int j=0; j<matrix[0].length; j++){
                 System.out.print(matrix[i][j]+" ");
             }
             System.out.println();
         }
+        searchKey(matrix, 5);
     }
 }
